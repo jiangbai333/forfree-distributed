51148a2a5a220b1c71d6cd46edf6409a31eb8f33
姜柏超 <jiangbai333@gmail.com>
2022-08-18 14:28:13 +0800

    💪  WIP: 添加payment子项目
    Signed-off-by: 姜柏超 <jiangbai333@gmail.com>

---
+++ distributed-payment/src/main/java/idni/jbc/forfree/distributed/payment/DistributedPaymentApplication.java
@@ -0,0 +1,13 @@
+package idni.jbc.forfree.distributed.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedPaymentApplication.class, args);
+	}
+
+}
---
+++ distributed-payment/src/test/java/idni/jbc/forfree/distributed/payment/DistributedPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package idni.jbc.forfree.distributed.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
